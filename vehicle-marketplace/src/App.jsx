155b7732bb05908d5f48e4efb872c6da99191d07
GERRY <gerrymnm@gmail.com>
2025-09-21 00:27:54 -0700

    fix(web): restore login/register and DealerDashboard route

--- vehicle-marketplace/src/App.jsx
+++ vehicle-marketplace/src/App.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MarketHome from "./pages/MarketHome.jsx";
 import Search from "./pages/Search.jsx";
 import VehicleDetails from "./pages/VehicleDetails.jsx";
 import DealerDashboard from "./pages/DealerDashboard.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 import { AuthProvider, RequireAuth } from "./lib/auth.jsx";
+import Header from "./components/Header.jsx";
 
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <nav style={{ padding: 8 }}>
-          <Link to="/" style={{ marginRight: 12 }}>Vehicle Home</Link>
-          <Link to="/search">Search</Link>
-          <span style={{ float: "right" }}><Link to="/dealer">Dealer</Link></span>
-        </nav>
+        <Header />
         <Routes>
           <Route path="/" element={<MarketHome />} />
           <Route path="/search" element={<Search />} />
           <Route path="/vehicles/:vin" element={<VehicleDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route
             path="/dealer"
             element={
